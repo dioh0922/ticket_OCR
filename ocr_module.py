71a139276e4044d8ddfc90386e733c9b94089421
dioh0922 <43507423+dioh0922@users.noreply.github.com>
2019-11-12 09:18:52 +0900

    抽出領域の描画追加

--- ocr_module.py
+++ ocr_module.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from PIL import ImageDraw
 import pyocr
 import pyocr.builders
 import os
@@ -15,12 +16,29 @@ def ocr(target):
 	else:
 		try:
 			image = Image.open(target)
+
+			"""
 			t = tool[0].image_to_string(image,
 				lang="jpn",
 				builder=pyocr.builders.TextBuilder(tesseract_layout=6)
 				)
 
 			print(t)
+			"""
+			box = tool[0].image_to_string(image,
+				lang="jpn",
+				builder=pyocr.builders.LineBoxBuilder(tesseract_layout=6)
+				)
+
+			#描画処理用のインスタンスに矩形を描画
+			pos_img = ImageDraw.Draw(image)
+
+			for pos in box:
+				print("w:{}, pos:{}".format(pos.content, pos.position))
+				pos_img.rectangle(pos.position, outline="red", width=3)
+
+			#処理結果を表示(描画オブジェクトで書き込まれるのは元のオブジェクト)
+			image.show()
 
 		except Exception as e:
 			print(e)
